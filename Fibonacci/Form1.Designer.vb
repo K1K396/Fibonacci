3d350387add9f0f3e70b2073ce875ea7dac6913c
Usuario <Usuario@TOSHIBA>
2017-02-17 01:47:55 -0600

    Fibonacci (Listo)

--- Fibonacci/Form1.Designer.vb
+++ Fibonacci/Form1.Designer.vb
@@ -22,9 +22,63 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtParametro = New System.Windows.Forms.TextBox()
+        Me.btnFibonacci = New System.Windows.Forms.Button()
+        Me.txtRespuesta = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Castellar", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(238, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Serie de Fibonacci"
+        '
+        'txtParametro
+        '
+        Me.txtParametro.Location = New System.Drawing.Point(16, 36)
+        Me.txtParametro.Name = "txtParametro"
+        Me.txtParametro.Size = New System.Drawing.Size(230, 20)
+        Me.txtParametro.TabIndex = 1
+        '
+        'btnFibonacci
+        '
+        Me.btnFibonacci.Location = New System.Drawing.Point(92, 62)
+        Me.btnFibonacci.Name = "btnFibonacci"
+        Me.btnFibonacci.Size = New System.Drawing.Size(75, 23)
+        Me.btnFibonacci.TabIndex = 2
+        Me.btnFibonacci.Text = "Fibonacci"
+        Me.btnFibonacci.UseVisualStyleBackColor = True
+        '
+        'txtRespuesta
+        '
+        Me.txtRespuesta.Location = New System.Drawing.Point(16, 92)
+        Me.txtRespuesta.Name = "txtRespuesta"
+        Me.txtRespuesta.Size = New System.Drawing.Size(230, 20)
+        Me.txtRespuesta.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(258, 132)
+        Me.Controls.Add(Me.txtRespuesta)
+        Me.Controls.Add(Me.btnFibonacci)
+        Me.Controls.Add(Me.txtParametro)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Serie de Fibonacci"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtParametro As TextBox
+    Friend WithEvents btnFibonacci As Button
+    Friend WithEvents txtRespuesta As TextBox
 End Class
--- Fibonacci/Form1.vb
+++ Fibonacci/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
-
+    Function fibonacci(numero As Integer) As Integer
+        Dim anterior As Integer = 1
+        Dim siguiente As Integer = 1
+        Dim actual As Integer
+        Dim cont As Integer = 0
+        While cont <= numero
+            actual = anterior + siguiente
+            anterior = siguiente
+            siguiente = actual
+            cont = cont + 1
+        End While
+        Return actual
+    End Function
+    Private Sub btnFibonacci_Click(sender As Object, e As EventArgs) Handles btnFibonacci.Click
+        txtRespuesta.Text = fibonacci(txtParametro.Text)
+    End Sub
 End Class
